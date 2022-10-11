16b2fb22cffdbae4a0efc8d298117aff2703d22a
Eddy G <eddy.spambari@gmail.com>
2022-10-11 00:49:17 -0700

    more to add, not done

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-const newBook = document.querySelector(".newBook"); newBook.addEventListener('click', addBookToLibrary);
-let bookCounter = 0;
-    let myLibrary = [];
+let myLibrary = [];
+let numOfBooks = 0;
+const books = document.querySelector(".books");
+const bookButton = document.querySelector(".addBook");
+const author = document.querySelector("#author");
+const title = document.querySelector("#title");
+const pages = document.querySelector("#pages");
 
-function Book(title, author, pages, read) {
-    this.title = title;
+function Book(author, title, pages) {
     this.author = author;
+    this.title = title;
     this.pages = pages;
-    this.read = read;
+    this.addBookToLibrary = function() {
+        myLibrary.push(this);
+    }
+}
+function displayBook() {
+    for(let k = numOfBooks; k < myLibrary.length + 1; k++) {
+        const addNewBook = document.createElement('div');
+
+        const bookAuthor = document.createElement('h2');
+        bookAuthor.innerHTML = `Author: ${myLibrary[k].author}`;
+        addNewBook.appendChild(bookAuthor);
+
+        const bookTitle = document.createElement('h2');
+        bookTitle.innerHTML = `Title: ${myLibrary[k].title}`;
+        addNewBook.appendChild(bookTitle);
+
+        const bookPages = document.createElement('h2');
+        bookPages.innerHTML = `Pages: ${myLibrary[k].pages}`;
+        addNewBook.appendChild(bookPages);
+
+        addNewBook.classList.add("book");
+        books.appendChild(addNewBook);
+
+        numOfBooks++;
+    }
 }
 
-function addBookToLibrary() {
-     
+function createBook() {
+    let newBook = new Book(author.value, title.value, pages.value);
+    newBook.addBookToLibrary();
+    displayBook();
 }
+
+bookButton.addEventListener('click', createBook);
